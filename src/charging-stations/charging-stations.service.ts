3fb366eaef91ac8601c83528ba44d4a39b9b3275
kamwro <94471234+kamwro@users.noreply.github.com>
2023-12-21 06:54:04 +0100

    add charging stations service, clean up

--- src/charging-stations/charging-stations.service.ts
+++ src/charging-stations/charging-stations.service.ts
@@ -1,15 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ChargingStation } from './entities/charging-station.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ChargingStationsService {
-  async findAll() {}
-
-  async findOneBy(criteria: string, data: string | number) {
-    criteria;
-    data;
+  readonly #chargingStationRepository: Repository<ChargingStation>;
+  constructor(
+    @InjectRepository(ChargingStation)
+    chargingStationRepository: Repository<ChargingStation>,
+  ) {
+    this.#chargingStationRepository = chargingStationRepository;
   }
 
-  async create() {}
+  async findAll(): Promise<ChargingStation[]> {
+    return await this.#chargingStationRepository.find({ relations: ['charging_station_types'] });
+  }
 
-  async remove() {}
+  async findAllBy(
+    searchProperty: string,
+    searchCriteria: string,
+    limit: number = 20,
+    sortProperty: string = 'created_at',
+    sortDirection: 'ASC' | 'DESC' = 'DESC',
+  ): Promise<ChargingStation[]> {
+    return await this.#chargingStationRepository.find({
+      take: limit,
+      where: { [searchProperty]: searchCriteria },
+      order: { [sortProperty]: sortDirection },
+      relations: ['charging_station_types'],
+    });
+  }
+
+  async sortAllBy(limit: number = 20, sortProperty: string, sortDirection: 'ASC' | 'DESC' = 'DESC'): Promise<ChargingStation[]> {
+    return await this.#chargingStationRepository.find({
+      take: limit,
+      order: { [sortProperty]: sortDirection },
+      relations: ['charging_station_types'],
+    });
+  }
 }
